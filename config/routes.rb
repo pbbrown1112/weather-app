eb9315b1452e1b5f9fd2439f4a243189c1136604
Brendan Brown <pbbrown1112@gmail.com>
2019-07-25 16:55:34 -0500

    can access rails db through local host in json

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  scope '/api/v1' do
-    resources :user_cities
-    resources :cities
-    resources :users
-  end
+  
+      resources :users
+      resources :cities
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
