3cfaec46fadb5b307d50cee43c5521a7389c104b
Brendan Brown <pbbrown1112@gmail.com>
2019-07-25 15:41:20 -0500

    working on routes and apis

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :user_cities
-  resources :cities
-  resources :users
+  scope '/api/v1' do
+    resources :user_cities
+    resources :cities
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
